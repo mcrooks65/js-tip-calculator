4db60ae13d4eb5c25800ca41991574adde403eac
mcrooks65 <mfcrooks@gmail.com>
2021-10-12 18:47:10 -0700

    Wire up calcButton to run tipCalculateEvent function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log(`script.js is linked into index.html!`);
+
+var calcButton = document.getElementById("calcButton")
+
+var tipCalculateEvent = function()
+{
+    document.getElementById("Solution").innerText = "Click event works!"
+
+};
+
+calcButton.addEventListener("click", tipCalculateEvent);
