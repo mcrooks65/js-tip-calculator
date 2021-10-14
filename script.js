6736c895d8e7fb7fab37c5d1171e2a3658abd498
mcrooks65 <mfcrooks@gmail.com>
2021-10-13 17:16:08 -0700

    Update tipCalculateEvent function to calculate tip

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 console.log(`script.js is linked into index.html!`);
 
-var calcButton = document.getElementById("calcButton")
+var calcButton = document.getElementById("calcButton");
 
 var tipCalculateEvent = function()
 {
-    document.getElementById("Solution").innerText = "Click event works!"
+    var mealCost = document.getElementById("mealCost").value;
+    var tipPercent = document.getElementById("tipPercent").value;
+    var billPayers = document.getElementById("billPayers").value;
 
+    var tipAmount = Number(mealCost) * Number(tipPercent);
+    var totalCost = Number(mealCost) + tipAmount;
+
+    var solution = totalCost / billPayers;
+
+    document.getElementById("Solution").innerText = "Each person owes: " + solution;
 };
 
 calcButton.addEventListener("click", tipCalculateEvent);
