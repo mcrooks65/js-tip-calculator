e17412bcb760e221c5a4aea81855d39471de98ba
mcrooks65 <mfcrooks@gmail.com>
2021-10-13 17:20:50 -0700

    Round soluion to the first 2 decimal places

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var tipCalculateEvent = function()
 
     var solution = totalCost / billPayers;
 
-    document.getElementById("Solution").innerText = "Each person owes: " + solution;
+    document.getElementById("Solution").innerText = "Each person owes: " + solution.toFixed(2);
 };
 
 calcButton.addEventListener("click", tipCalculateEvent);
